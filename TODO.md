d081f6e28aaf84c5cab6f38defd9221b699feded
Sebastian Zientek <sebastian.zientek1@gmail.com>
2023-11-08 18:27:00 +0100

    Removing leftovers and preparing new tasks in TODO

--- TODO.md
+++ TODO.md
@@ -1,7 +1,9 @@
-# Refactor of ReadingStorage and things around
+# Webpage compiled into the sources instead of loading from SD
 
 # Prepare UTs
 
 # Warning on webpage if data from sensor was readed earlier than update_time * 2
 
 # Sending "updateTime" for sensors during pairing
+
+# Sending web event periodically instead of on event from sensor
--- src/ReadingsStorage.cpp
+++ src/ReadingsStorage.cpp
@@ -10,9 +10,6 @@ void ReadingsStorage::addReading(const MacAddr &mac,
                                  float humidity,
                                  unsigned long epochTime)
 {
-    auto fracVal = temperature - static_cast<int>(temperature);  // NOLINT
-    int8_t frac = fracVal > 0.5 ? 1 : 0;                         // NOLINT
-
     Reading reading = {temperature, humidity, epochTime};
     m_readings[mac] = reading;
 
