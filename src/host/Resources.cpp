e68e5c5d007af2e4e6d961e9ce2bbc74273b489c
Sebastian Zientek <sebastian.zientek1@gmail.com>
2024-02-07 22:42:49 +0100

    Separation Resources from WebViews

--- src/host/Resources.cpp
+++ src/host/Resources.cpp
@@ -1,3 +1,5 @@
+#include "Resources.hpp"
+
 #include <incbin.h>
 
 INCTXT(IndexHtml, "src/host/html/index.html");
@@ -6,3 +8,33 @@ INCTXT(MicroChart, "src/host/html/microChart.js");
 INCBIN(Favicon, "src/host/html/fav.png");
 
 INCTXT(WifiSettingsHtml, "src/host/html/wifiSettings.html");
+
+const char *Resources::getIndexHtml()
+{
+    return gIndexHtmlData;
+}
+
+const char *Resources::getAdminHtml()
+{
+    return gAdminHtmlData;
+}
+
+const char *Resources::getMicroChart()
+{
+    return gMicroChartData;
+}
+
+const unsigned char *Resources::getFavicon()
+{
+    return gFaviconData;
+}
+
+unsigned int Resources::getFaviconSize()
+{
+    return gFaviconSize;
+}
+
+const char *Resources::getWifiSettingsHtml()
+{
+    return gWifiSettingsHtmlData;
+}
