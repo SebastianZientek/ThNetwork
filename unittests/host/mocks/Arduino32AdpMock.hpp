9cdb8e2b41c0835c45842767d36c2961128ffa6b
Sebastian Zientek <sebastian.zientek1@gmail.com>
2024-02-18 12:17:24 +0100

    Fix mocks (#47)

--- unittests/host/mocks/Arduino32AdpMock.hpp
+++ unittests/host/mocks/Arduino32AdpMock.hpp
@@ -11,42 +11,42 @@
 class Arduino32AdpMock : public IArduino32Adp
 {
 public:
-    void pinMode(uint8_t pin, IArduino32Adp::Mode mode)
+    void pinMode(uint8_t pin, Mode mode) override
     {
-        mock()
-            .actualCall("Arduino32Adp::digitalWrite")
+        mock("Arduino32Adp")
+            .actualCall("digitalWrite")
             .withParameter("pin", pin)
             .withParameter("mode", static_cast<int>(mode));
     }
 
-    bool digitalRead(uint8_t pin)
+    bool digitalRead(uint8_t pin) override
     {
-        return mock()
-            .actualCall("Arduino32Adp::digitalRead")
+        return mock("Arduino32Adp")
+            .actualCall("digitalRead")
             .withParameter("pin", pin)
             .returnBoolValueOrDefault(false);
     }
 
-    void digitalWrite(uint8_t pin, bool val)
+    void digitalWrite(uint8_t pin, bool val) override
     {
-        mock()
-            .actualCall("Arduino32Adp::digitalWrite")
+        mock("Arduino32Adp")
+            .actualCall("digitalWrite")
             .withParameter("pin", pin)
             .withParameter("val", val);
     }
 
-    uint8_t getLedBuiltin()
+    uint8_t getLedBuiltin() override
     {
-        return mock().actualCall("Arduino32Adp::getLedBuiltin").returnIntValueOrDefault(0);
+        return mock("Arduino32Adp").actualCall("getLedBuiltin").returnIntValueOrDefault(0);
     }
 
-    unsigned long millis()
+    unsigned long millis() override
     {
-        return mock().actualCall("Arduino32Adp::millis").returnIntValueOrDefault(0);
+        return mock("Arduino32Adp").actualCall("millis").returnIntValueOrDefault(0);
     }
 
-    void delay(unsigned long milliseconds)
+    void delay(unsigned long milliseconds) override
     {
-        mock().actualCall("Arduino32Adp::delay").withParameter("milliseconds", milliseconds);
+        mock("Arduino32Adp").actualCall("delay").withParameter("milliseconds", milliseconds);
     }
 };
